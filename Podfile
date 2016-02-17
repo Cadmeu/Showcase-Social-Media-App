d597ba9d6d21c395b5a001779bf5a81b30b5e527
Cade Meurer <clmeurer94@gmail.com>
2016-02-17 16:31:22 -0500

    Added Facebook Framework/Api for logins

---
+++ Podfile
@@ -0,0 +1,7 @@
+use_frameworks!
+
+target 'Showcase-SocialMedia' do
+pod 'Firebase', '>= 2.5.0'
+
+end
+
---
+++ Pods/Target Support Files/Pods-Showcase-SocialMedia/Pods-Showcase-SocialMedia-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Showcase_SocialMediaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Showcase_SocialMediaVersionString[];
+
